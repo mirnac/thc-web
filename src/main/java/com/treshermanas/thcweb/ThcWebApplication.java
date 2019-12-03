76a603a829af29a267d0f2df3701d61509a83f48
Mirna Cantero <mirnacantero@gmail.com>
2019-12-03 16:46:04 -0300

    Cambios finales para la puesta en producción.

--- src/main/java/com/treshermanas/thcweb/ThcWebApplication.java
+++ src/main/java/com/treshermanas/thcweb/ThcWebApplication.java
@@ -2,9 +2,10 @@ package com.treshermanas.thcweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThcWebApplication {
+public class ThcWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ThcWebApplication.class, args);
