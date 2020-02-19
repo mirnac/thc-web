4fa9ab1c84cfbc3bd7caf1d2797120f892ac4be0
Mirna Cantero <mirnacantero@gmail.com>
2020-02-19 17:14:47 -0300

    Renamed directory to english for names consistency.

--- src/main/java/com/treshermanas/thcweb/config/MvcConfig.java
+++ src/main/java/com/treshermanas/thcweb/config/MvcConfig.java
@@ -18,7 +18,8 @@ public class MvcConfig implements WebMvcConfigurer {
 
         registry.addViewController("/payments/report/go").setViewName("payments/payments_report");
         registry.addViewController("/invoices/unpaid/report/go").setViewName("invoices/unpaid_report");
-        registry.addViewController("/pedidos/tablero").setViewName("productos/pedidos");
+        registry.addViewController("/products/details/search/init").setViewName("products/search");
+        registry.addViewController("/pedidos/tablero").setViewName("products/pedidos");
     }
 }
 
